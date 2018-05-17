4a3ae4217ce38ba9b9d6984c8387099be93ad6b1
Veselin Hr. Trayanov <1.1@gmx.com>
2018-05-17 21:49:40 +0100

    Update Footer.jsx

--- mimo/src/components/common/Footer.jsx
+++ mimo/src/components/common/Footer.jsx
@@ -13,14 +13,12 @@ export class Footer extends React.Component {
                     <li className="socialIcon"><a href="#" target="_blank" className="fa fa-github"></a></li>
                     <li className="socialIcon"> <a href="#" target="_blank"className="fa fa-twitter"></a></li>
                </ul>
-                  
-                     <span id="copy">© 2017 Copyright</span> 
-                
-                     <h4>London office</h4> 
-                     <p>52 Battersea Park Road, </p> 
-                     <p>Battersea, London</p>
-                     <span>SW11 4JP</span>
-                     <a id="phone-footer" className="phone-link-yellow" href="tel:07428-390052">07428 390052</a>
+                    <span id="copy">© 2017 Copyright</span> 
+                    <h4>London office</h4> 
+                    <p>52 Battersea Park Road, </p> 
+                    <p>Battersea, London</p>
+                    <span>SW11 4JP</span>
+                    <a id="phone-footer" className="phone-link-yellow" href="tel:07428-390052">07428 390052</a>
             </footer>
         );
     }
